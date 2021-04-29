be6e006d5ce23945bf7d7d68efb71a84572f00ab
baohuixiao <17705032736@163.com>
2021-04-29 15:51:56 +0800

    fix:删除多余命令

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ const initAction = () => {
         message: '请输入项目名称：',
         name: 'projectName'
     }]).then(answers => {
-        const {projectName} = answers;
+        const {
+            projectName
+        } = answers;
         console.log(`项目名为：${projectName}`)
         console.log('正在拷贝项目，请稍等...')
         let giturl = 'https://github.com/baohuixiao/h5-gulp-template.git';
         download(giturl, `./${projectName}`, () => {
-            shell.rm('-rf', `${projectName}/.git`) // 移除项目中的.git文件
             shell.cd(projectName) // 进入项目
+            shell.rm('-rf', '.git') // 移除项目中的.git文件
             shell.exec('npm install') // 运行 npm install
             // 操作提示
             console.log(`
@@ -42,23 +44,6 @@ program
     .description('初始化项目')
     .action(initAction)
 
-program.command('new <name>')
-    .description('创建项目')
-    .action(name => {
-        let giturl = 'https://github.com/vuejs/vue-next-webpack-preview.git';
-        download(giturl, `./${name}`, () => {
-            shell.rm('-rf', `${name}/.git`) // 移除项目中的.git文件
-            shell.cd(name) // 进入项目
-            shell.exec('npm install') // 运行 npm install
-            // 操作提示
-            console.log(`
-                创建项目${name}成功
-                cd ${name} 进入项目
-                mycli run 启动项目
-                mycli start 预览项目
-            `)
-        })
-    })
 program.command('run')
     .description('运行项目')
     .action(name => {
@@ -70,11 +55,13 @@ program.command('run')
         //     console.log('项目启动成功')
         // })
     })
+
 program.command('start')
     .description('预览项目')
     .action(name => {
         open('http://localhost:7777/')
     })
+
 /**
  * program.parse 是将命令参数传入commander管道中，一般放在最后执行
  * process.argv 可以获取到命令的参数，以数组返回
